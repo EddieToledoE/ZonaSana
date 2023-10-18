9bcb0deb5ea58f7ec8a7166e0aca5e1bf734778a
Eddie Toledo <teddyrepollo@gmail.com>
2023-10-18 12:17:45 -0600

    primer commit con pocos avances, en este caso unicamente componentes sueltos

---
+++ pages/Grafica.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Styles from '../styles/Grafica.css';
+
+
+
+function Grafica(){
+
+
+    return(
+            <div className="Grafica-Principal">
+
+
+                <h1> Este es la tabla</h1>
+
+
+
+
+            </div>
+
+
+
+
+
+    ); 
+}
+export default Grafica;

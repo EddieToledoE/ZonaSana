3b12972e9c8d4690df6072313f50f6fedf9b559d
unknown <Teddy_03@outlook.es>
2023-11-23 08:14:10 -0600

    minuscula correccion

--- app/api/auth/cita/conteo/route.js
+++ app/api/auth/cita/conteo/route.js
@@ -3,16 +3,18 @@ import { NextResponse } from "next/server";
 import Cita from "@/models/Cita";
 
 export async function GET() {
-    connectarBD();
-    try {
+  connectarBD();
+  try {
     const cantidadCitas = await Cita.countDocuments();
-    const cantidadCitasPendientes = await Cita.countDocuments({pendientes : true});
-      return NextResponse.json({cantidadCitas, cantidadCitasPendientes});
-    } catch (error) {
-      console.error("Error al obtener citas:", error);
-      return NextResponse.error({
-        statusCode: 500,
-        message: "Error del servidor",
-      });
-    }
+    const cantidadCitasPendientes = await Cita.countDocuments({
+      pendiente: true,
+    });
+    return NextResponse.json({ cantidadCitas, cantidadCitasPendientes });
+  } catch (error) {
+    console.error("Error al obtener citas:", error);
+    return NextResponse.error({
+      statusCode: 500,
+      message: "Error del servidor",
+    });
   }
+}
